7be4ba2f93540855b1473158a5cbcbc8b22b28ea
tfkfan <sylar131@live.ru>
2017-12-08 19:14:43 +0300

    Init spring framework

--- src/main/java/com/tfkfan/entry/Entry.java
+++
@@ -1,9 +0,0 @@
-package com.tfkfan.entry;
-
-public class Entry {
-
-	public static void main(String[] args) {
-		System.out.println("Hi");
-	}
-
-}
---
+++ src/main/java/com/tfkfan/mvc/TestController.java
@@ -0,0 +1,14 @@
+package com.tfkfan.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping(value= "/", method = RequestMethod.GET)
+	public String home(){
+		return "home";
+	}
+}
