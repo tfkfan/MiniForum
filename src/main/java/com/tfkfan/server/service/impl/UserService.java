d1bc23ca68fe80740b758f500267cfcf961e25f9
tfkfan <sylar131@live.ru>
2017-12-14 17:12:48 +0300

    Little fix

--- src/main/java/com/tfkfan/server/service/impl/UserService.java
+++ src/main/java/com/tfkfan/server/service/impl/UserService.java
@@ -1,7 +1,5 @@
 package com.tfkfan.server.service.impl;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -10,15 +8,9 @@ import com.tfkfan.hibernate.dao.UserDao;
 import com.tfkfan.hibernate.entities.Role;
 import com.tfkfan.hibernate.entities.User;
 import com.tfkfan.security.enums.UserRole;
-import com.tfkfan.server.service.IService;
-import com.tfkfan.server.service.exception.IncorrectUsernameException;
-import com.tfkfan.server.service.exception.UsernameAlreadyExistsException;
 
 @Service("userService")
-public class UserService implements IService<User> {
-
-	@Autowired
-	UserDao userDao;
+public class UserService extends AbstractService<User> {
 
 	@Autowired
 	PasswordEncoder pe;
@@ -32,20 +24,9 @@ public class UserService implements IService<User> {
 	public void initRole() {
 		userRole = roleDao.getRoleByName(UserRole.ROLE_USER.getRole());
 	}
-
-	@Override
-	public void update(User obj) throws Exception {
-		userDao.update(obj);
-	}
-
-	@Override
-	public void add(User obj) throws Exception {
-		if (obj.getUsername() == null || obj.getUsername().isEmpty())
-			throw new IncorrectUsernameException();
-		User user = userDao.findByUsername(obj.getUsername());
-		if (user != null)
-			throw new UsernameAlreadyExistsException(obj.getUsername());
-		userDao.save(obj);
+	
+	public UserService(){
+		
 	}
 
 	public void createNewUser(String username, String password) throws Exception {
@@ -57,24 +38,6 @@ public class UserService implements IService<User> {
 		user.setPassword(password);
 		user.setRole(userRole);
 
-		add(user);
+		((UserDao)getDao()).createUser(user);
 	}
-
-	@Override
-	public void delete(User obj) throws Exception {
-		userDao.delete(obj);
-	}
-
-	@Override
-	public User get(Long id) throws Exception {
-		if(id == null)
-			throw new NullPointerException("Id is null");
-		return userDao.get(id);
-	}
-
-	@Override
-	public List<User> getAll() throws Exception {
-		return userDao.listAll();
-	}
-
 }
